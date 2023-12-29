d1d9628b8bdf46d99f2a2c04374d8cc0f5b2253d
Amir <140071494+Crimson-Amir@users.noreply.github.com>
2023-12-30 00:31:18 +0330

    Update config-tunnel.py

--- config-tunnel.py
+++ config-tunnel.py
@@ -3,34 +3,34 @@ import subprocess
 
 
 def run_command(command):
-    try:
-        subprocess.run(command, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Command execution failed: {e}")
-        exit(1)
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Command execution failed: {e}")
+        exit(1)
 
 
 # Get IP_KHAREJ and IP_IRAN from user input
 try:
-    IP_IRAN = socket.gethostbyname(socket.gethostname())
-    IP_KHAREJ = input("Enter IP_KHAREJ: ")
+    IP_IRAN = socket.gethostbyname(socket.gethostname())
+    IP_KHAREJ = input("Enter IP_KHAREJ: ")
 except KeyboardInterrupt:
-    print("\nUser interrupted the input.")
-    exit(1)
+    print("\nUser interrupted the input.")
+    exit(1)
 except socket.gaierror as e:
-    print(f"Failed to retrieve the IP address: {e}")
-    exit(1)
+    print(f"Failed to retrieve the IP address: {e}")
+    exit(1)
 
 # Execute the commands on the server
 commands = [
-    "sysctl net.ipv4.ip_forward=1",
-    f"iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination {IP_IRAN}",
-    f"iptables -t nat -A PREROUTING -j DNAT --to-destination {IP_KHAREJ}",
-    "iptables -t nat -A POSTROUTING -j MASQUERADE"
+    "sysctl net.ipv4.ip_forward=1",
+    f"iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination {IP_IRAN}",
+    f"iptables -t nat -A PREROUTING -j DNAT --to-destination {IP_KHAREJ}",
+    "iptables -t nat -A POSTROUTING -j MASQUERADE"
 ]
 
 for command in commands:
-    run_command(command)
+    run_command(command)
 
 # Create and write the contents to /etc/rc.local
 rc_local_contents = """\
@@ -46,11 +46,11 @@ rc_local_contents = rc_local_contents.replace("{IP_IRAN}", IP_IRAN)
 rc_local_contents = rc_local_contents.replace("{IP_KHAREJ}", IP_KHAREJ)
 
 try:
-    with open('/etc/rc.local', 'w') as file:
-        file.write(rc_local_contents)
+    with open('/etc/rc.local', 'w') as file:
+        file.write(rc_local_contents)
 except IOError as e:
-    print(f"Error writing to /etc/rc.local: {e}")
-    exit(1)
+    print(f"Error writing to /etc/rc.local: {e}")
+    exit(1)
 
 # Give root access to /etc/rc.local
 run_command("sudo chmod +x /etc/rc.local")
@@ -73,11 +73,11 @@ WantedBy=multi-user.target
 """
 
 try:
-    with open('/etc/systemd/system/update-resolvconf.service', 'w') as file:
-        file.write(update_resolvconf_contents)
+    with open('/etc/systemd/system/update-resolvconf.service', 'w') as file:
+        file.write(update_resolvconf_contents)
 except IOError as e:
-    print(f"Error writing to /etc/systemd/system/update-resolvconf.service: {e}")
-    exit(1)
+    print(f"Error writing to /etc/systemd/system/update-resolvconf.service: {e}")
+    exit(1)
 
 # Enable the update-resolvconf.service
 run_command('sudo systemctl enable update-resolvconf.service')
